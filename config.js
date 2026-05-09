597637d726f7b1f6d497299e3cd4da2645259ef2
kikimyomyo <request1004@gmail.com>
2026-05-09 13:17:26 +0900

    deploy: genesis-hub

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "genesis-hub",
+  title: "창세기 학습 게임 — 메인",
+  sheet_tab: "genesis-hub",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
